6ac3be2def0e23dee4b3cb8453b8d07f9ea3e0da
Garrett Lee <rgl10d@gmail.com>
2020-10-07 15:33:21 -0400

    save button now saves text to local storage

--- script.js
+++ script.js
@@ -1,46 +1,48 @@
-$(document).ready(function (){
-    var now = moment();
-    var hour = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"];
+$(document).ready(function () {
+  // JS Variables
+  var now = moment();
+  var hour = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM",];
 
-    // Function Definitions
-    
-    function lineRender(){
-        for(var i = 0; i < hour.length; i++){
-            var row = $("<div>").attr("class", "row");
-            var hourEl = $("<div>").attr("class", "hour").text(hour[i]);
-            var textArea = $("<textarea>");
-            var saveBtn = $("<button>").attr("class", "saveBtn").text("Save");
-            var parsedHour = moment(hour[i], "hh a");
+  // Function Definitions
+  function lineRender() {
+    for (var i = 0; i < hour.length; i++) {
+      var textboxId = "textbox" + i;
+      var buttonId = "btn" + i;
+      var row = $("<div>").attr("class", "row time-block");
+      var hourEl = $("<div>").attr("class", "hour").text(hour[i]);
+      var textArea = $("<textarea>").attr("id", textboxId);
+      var saveBtn = $("<button>")
+        .attr({
+          "class": "saveBtn",
+          "id": buttonId,
+          "onClick": "saveText('" + textboxId + "')"
+        })
+        .text("Save");
+      var parsedHour = moment(hour[i], "hh a");
+      var appointment = localStorage.getItem(textboxId);
 
-            if(now.hour() === parsedHour.hour()){
-                textArea.attr("class", "present");
-            } else if(now.hour() < parsedHour.hour()){
-                textArea.attr("class", "future");
-            } else if(now.hour() > parsedHour.hour()){
-                textArea.attr("class", "past");
-            }
+      if (now.hour() === parsedHour.hour()) {
+        textArea.attr("class", "present");
+      } else if (now.hour() < parsedHour.hour()) {
+        textArea.attr("class", "future");
+      } else if (now.hour() > parsedHour.hour()) {
+        textArea.attr("class", "past");
+      }
 
-            $("#schedule").append(row);
-            row.append(hourEl);
-            row.append(textArea);
-            row.append(saveBtn);
-        }
+      $("#schedule").append(row);
+      row.append(hourEl);
+      row.append(textArea);
+      row.append(saveBtn);
+      $(textboxId).text(appointment);
+      
     }
+  }
 
-    // Function calls
-    $("#currentDay").text(now.format("MMM Do YYYY"));
-    lineRender();
+  // Function calls
+  $("#currentDay").text(now.format("MMM Do YYYY"));
+  lineRender();
+});
 
-    // Event Listeners
-    $()
-})
-
-/**
- * create coluimn to hold time
- */
-// check to see current time and add class accordingly
-// var hournow = parseInt(time.timeNumber);
-// consolelog(hournow);
-// if( hourNow === parseInt(timeNow)) {
-//     textContent.addClass("present")
-// } else if (hourNow <= parseInt(timeNow))
+function saveText(textboxId) {
+    localStorage.setItem(textboxId, $("#" + textboxId).val());
+  }
